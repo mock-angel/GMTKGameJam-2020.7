5742172905d33f0bdc99b57f851a7c8d38befec9
Anantha Krishna R <sujakrishnar@gmail.com>
2020-07-12 00:05:06 +0530

    FIx #42: Done bring back all lost changes. Master is good.

---
+++ GMTKGameJam-2020.7/Assets/Scripts/Potion/PotionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionManager : MonoBehaviour
+{
+    public static PotionManager Instance {set; private get;}
+    
+    public List<Potion> potionList;
+
+    public bool isSpellActive;
+
+    void Update(){
+
+        if(isSpellActive) return;
+
+        for(int i = 0; i< potionList.Count; i++){
+            if (Input.GetKeyDown(potionList[i].keyCode))
+            {
+                // do some stuff here
+                potionList[i].CasteSpell();
+            }
+        }
+    }
+
+}
