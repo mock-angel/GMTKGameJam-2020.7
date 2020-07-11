4a7642ab28e3e8f68539bdc6cd956b65cbaf2fb3
Anantha Krishna R <sujakrishnar@gmail.com>
2020-07-11 16:51:02 +0530

    Fix #26: Enemy spawner now spawns enemies every tick. Basic enemy added.

--- GMTKGameJam-2020.7/Assets/Scripts/EnemyController.cs
+++ GMTKGameJam-2020.7/Assets/Scripts/EnemyController.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 2f;
     
-    //public Rigidbody2D rb;
     public Animator animator;
     
     Vector3 movement;
@@ -21,24 +20,16 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
-
-
         if (Mathf.Abs(axis.x) == 1 && Mathf.Abs(axis.y) == 1)
         {
             movement.x = Mathf.Sqrt(0.5f) * axis.x;
             movement.y = Mathf.Sqrt(0.5f) * axis.y;
         }
         
-        //Fixed update script
-        
-//        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-//        Vector2 addForce = new Vector2(movement.x * moveSpeed * Time.deltaTime, movement.y * moveSpeed * Time.deltaTime);
-//        rb.AddForce(addForce);
     }
     
     void FixedUpdate()
     {   
         transform.position = transform.position + (movement * moveSpeed * Time.fixedDeltaTime);
-//        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
