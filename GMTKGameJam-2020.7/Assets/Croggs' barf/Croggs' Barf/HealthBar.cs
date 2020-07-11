79b4ac192428a362957997e353e4cd4b815707f7
CROGGS1 <67976211+CROGGS1@users.noreply.github.com>
2020-07-10 22:09:22 -0400

    Added potions?

---
+++ GMTKGameJam-2020.7/Assets/Croggs' barf/Croggs' Barf/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public RectTransform Health;
+    public static RectTransform HEALTH;
+
+    static public float HP = 100;
+
+    static float w, fullx, emptyx;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HEALTH = Health;
+        w = Health.sizeDelta.x;
+
+        fullx = Health.localPosition.x;
+        emptyx = Health.localPosition.x - w;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public static void Damage(float D)
+    {
+        HP =- D;
+
+        HEALTH.localPosition -= new Vector3(w * (100 / D),0,0);
+
+
+    }
+}
---
+++ GMTKGameJam-2020.7/Assets/Croggs' barf/Croggs' Barf/JamPage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JamPage : MonoBehaviour
+{
+
+    public Transform Main, Page;
+    static bool tog;
+
+    static Vector3 Active, Innactive;
+
+    public bool gotojam,leave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Active = Main.localPosition;
+        Innactive = Page.localPosition;
+    }
+
+    public void Pressed()
+    {
+        if (leave)
+        {
+            Application.Quit();
+        }
+
+
+        if (!gotojam)
+        {
+            tog = !tog;
+
+            Main.localPosition = (!tog ? Active : Innactive);
+            Page.localPosition = (tog ? Active : Innactive);
+        }
+        else
+        {
+            Application.OpenURL("https://itch.io/jam/gmtk-2020/entries");
+        }
+        
+
+
+
+
+    }
+}
